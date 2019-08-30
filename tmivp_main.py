e8883a7cc2f52d6415ea3c5e17545fac81149cdb
Karina Ramos-Musalem <kramosmu@eoas.ubc.ca>
2019-08-30 11:55:54 -0700

    module and main script in working form before lunch.

--- tmivp_main.py
+++ tmivp_main.py
@@ -1,8 +1,11 @@
-import template_matching
+import template_matching as tm
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.io  # only needed for reading in .mat file
+from sklearn.metrics import mean_squared_error
+from scipy.optimize import curve_fit
 
-# template_num = 1  # exponential
+#template_num = 1  # exponential
 template_num = 2  # gaussian
 # template_num = 3  # step
 cut_range = 2
@@ -17,18 +20,24 @@ lon = np.squeeze(mat.get('lon'))
 nstations = len(lat)
 del(s, mat)  # clean up workspace
 
+
 # Use cross-correlation to find location of template in each profile
 # and calculate mse between template and profile chunk
 MSE = np.zeros((nstations, 1))
 for i in range(nstations):
-    chunk_value, chunk_depth = find_correlation(t[:, i], z[:, i], template_num, cut_range)
+    t_prof = t[:, i]
+    z_prof = z[:, i]
+    t_prof = t_prof[np.isfinite(t_prof)]
+    z_prof = z_prof[np.isfinite(z_prof)]
+
+    chunk_value, chunk_depth = tm.find_correlation(t_prof, z_prof, template_num, cut_range)
     if template_num == 1:
-        MSE[i] = fit_exp(chunk_value, chunk_depth)
+        MSE[i] = tm.fit_exp(chunk_value, chunk_depth)
     elif template_num == 2:
-        MSE[i] = fit_gauss(chunk_value, chunk_depth)
+        MSE[i] = tm.fit_gauss(chunk_value, chunk_depth)
     else:
         print('Function for fitting this template is not yet available')
-
+    print(i)
 plt.scatter(np.arange(nstations), np.sqrt(MSE))
 plt.xlabel('Profile')
 plt.ylabel('RMSE')
